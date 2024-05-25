536079c975c8660f3c7d07818d076acd17a86147
BayramBurakZ <146845106+BayramBurakZ@users.noreply.github.com>
2024-05-25 18:26:40 +0200

    Delete process.py

--- process.py
+++
@@ -1,6 +0,0 @@
-class DataProcessor:
-    def process(self, data):
-
-        time, coordinates, action = data
-        processed_data = (timestamp, value.lower())  # Beispielverarbeitung
-        return processed_data
